8c83836653d5466da0ace621e66c952f80c1a68b
Jeffrey Vandenborne <jeffrey@vandenborne.co>
2021-05-06 14:37:08 +0200

    feat: install build dependencies before building RPM
    Ticket: TME-27303

--- src/main.ts
+++ src/main.ts
@@ -74,6 +74,7 @@ async function run() {
 
     // Execute rpmbuild , -ba generates both RPMS and SPRMS
     try {
+      await exec.exec(`yum-builddep -y ${specFile}`);
       await exec.exec(`rpmbuild -ba /github/home/rpmbuild/SPECS/${specFile}`);
     } catch (err) {
       core.setFailed(`action failed with error: ${err}`);
